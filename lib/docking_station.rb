9be5b4ec3b8b8065fd5a460a020938ea7ac68df0
camilla <cgreene553@gmail.com>
2022-01-26 18:00:12 +0000

    updated docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,12 @@
 class DockingStation
+  docking_station = DockingStation.new
   def release_bike
-bike = Bike.new
+# bike = Bike.new
   end
 end
 
-class Bike
-    def working?
+# class Bike
+#     def working?
 
-    end
-end
+#     end
+# end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,6 @@
 require 'docking_station'
 describe DockingStation do 
-    it "should release a bike" do
+    it "responds to release_bike" do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike)
     end
