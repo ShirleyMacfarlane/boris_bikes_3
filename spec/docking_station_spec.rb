866e8cb53cbc76c8a2681a65de0077f0118dc46b
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-01-26 16:58:31 +0100

    DockingStation class created

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+# describe DockingStation do 
+#     it "should release a bike" do
+#     docking_station = DockingStation.new
+#     expect(docking_station).to respond_to(:release).with(1).argument
+#     end
+# end
+
+require 'docking_station'
+
+describe DockingStation do
+end
