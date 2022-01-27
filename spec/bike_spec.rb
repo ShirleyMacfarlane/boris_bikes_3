b886a65c12b42562575ffc1547f7fe7408124d29
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-01-27 16:34:06 +0100

    Completed step 10.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,10 @@
 require 'bike'
 describe Bike do
+    # it "respond to working?" do
+    # bike = Bike.new 
+    # expect(bike).to respond_to(:working?)
     it { is_expected.to respond_to :working? }
+    # end
 end
+
+# it { is_expected.to respond_to :working? }
