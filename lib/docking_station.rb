92fbf7d5feb711fe180ce7f55163ed1dcf949292
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-01-28 16:33:54 +0000

    Updated Step 13

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,11 @@ class DockingStation
   end
   def release_bike
     #bike = Bike.new
-    fail "No bikes in Docking station" unless @dock_bikes.length > 0
-    return @dock_bikes[@dock_bikes.length - 1]
+    fail "No bikes in Docking station" if @dock_bikes.length == 0
+    return @dock_bikes.pop()
   end
   def dock_bike(bike)
-    @dock_bikes << bike
-    bike
+    fail "Docking station is full" if  @dock_bikes.count >= 20
+    return @dock_bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,17 @@
-require './lib/docking_station'
+require_relative '../lib/docking_station'
 describe DockingStation do 
      it "responds to release_bike" do
       docking_station = DockingStation.new
       expect(docking_station).to respond_to(:release_bike)
    end
-    it "responds to release_bike with a working? bike" do
-      docking_station = DockingStation.new
+  #  it "responds to release_bike with a working? bike" do
       # Need to put a bike in now as we don't always return a bike chapter Step 12
-      bike = Bike.new
-      docking_station.dock_bike(bike)
+  #    bike = Bike.new
+     # bikessubject.dock_bike(bike)
       #
-      bike = docking_station.release_bike
-      expect(bike).to respond_to(:working?)
-    end
+     # bike = subject.release_bike
+    #  expect(bike).to respond_to(:working?).to eq true
+   # end
     it "responds to docking a bike" do
       docking_station = DockingStation.new
       expect(docking_station).to respond_to(:dock_bike)
@@ -24,9 +23,8 @@ describe DockingStation do
       expect(docking_station).to respond_to(:dock_bike).with(1).argument
     end  
     it "responds to the passing of a new bike into method dock_bike" do
-      docking_station = DockingStation.new
       bike = Bike.new
-      expect(docking_station.dock_bike(bike)).to eq bike
+      expect(subject.dock_bike(bike)).to include(bike)
     end
     it "responds to checking that a bike has been docked" do
       docking_station = DockingStation.new
@@ -35,7 +33,13 @@ describe DockingStation do
       expect (:dock_bikes.length > 0 == true)
     end
     it "Error if no bikes to release" do
-      docking_station = DockingStation.new
-      expect{docking_station.release_bike}.to raise_error("No bikes in Docking station")
+      expect{subject.release_bike}.to raise_error("No bikes in Docking station")
+    end
+    it "Error if docking staion can't execpt anymore bikes" do
+      bike = Bike.new
+      20.times {subject.dock_bike Bike.new}
+     # bike = Bike.new
+     # subject.dock_bike(bike)
+      expect{subject.dock_bike(Bike.new)}.to raise_error("Docking station is full")
     end
 end
